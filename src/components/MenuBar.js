7f546231e87cbe529e58ffbad7f50f44ff644146
clheard54 <clarion.heard@gmail.com>
2020-04-03 10:16:00 -0700

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,19 +15,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={(event) => props.onSelectPage(event.target.id)} className= {props.selection == "profile" ? "item active" : "item"} id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={(event) => props.onSelectPage(event.target.id)} className= {props.selection == "photo" ? "item active" : "item"} id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={(event) => props.onSelectPage(event.target.id)} className= {props.selection == "cocktail" ? "item active" : "item"} id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={(event) => props.onSelectPage(event.target.id)} className= {props.selection == "pokemon" ? "item active" : "item"} id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -3,21 +3,38 @@ import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selection: 'profile'
+    }
+  }
 
+  selectPage = (id) => {
+    this.setState({
+      selection: id
+    })
+  }
 
   render() {
 
-    /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
-    */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+  let detailsToDisplay = <div>Hi, I'm a div!</div>
+    if (this.state.selection == "profile"){
+      detailsToDisplay = <Profile />
+    } 
+    if (this.state.selection == "photo"){
+        detailsToDisplay = <Photos />
+      }
+    if (this.state.selection == "cocktail"){
+        detailsToDisplay = <Cocktails />
+  }
+    if (this.state.selection == "pokemon"){
+      detailsToDisplay = <Pokemon />
+    }
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar selection={this.state.selection} onSelectPage={this.selectPage} />
         {detailsToDisplay}
       </div>
     )
